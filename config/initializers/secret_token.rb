ae4c51b8595a8e3be63b286af1291c53a868c786
Kai Kunze <kai.kunze@gmail.com>
2011-07-06 22:16:55 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kgartenblog::Application.config.secret_token = 'a36c4b5b1b8c4576e2863732dcf8c5aa78979b698fa1b9b08eb4342faa98f586a02f3b00212dcc3520e2afa138a93da2f12fbfb5f9a0cc50097ff996bca6b746'
